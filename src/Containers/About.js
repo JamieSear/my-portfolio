d38863da0ef50352d665ebb4a28f007994d24a7c
Jamie Sear <jamiesear@Jamie-Sears-MacBook.local>
2020-10-07 16:56:54 +0100

    Improved styling and included content to about page

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -16,21 +16,29 @@ function About(props) {
        <p className="aboutOneline">Hello, my name is Jamie Sear, I am a recent Digital Media graduate from the University of Leeds. I am currently learning to become a Full-Stack Web Developer at futureproof.</p>
     </div>
 
-        <h1 className="skillsTitle">My Skills:</h1>
+        <h1 className="skillsTitle">MY SKILLS & EXPERIENCE:</h1>
 <div className="about-wrapper">
 <div className="about-card">
-	<h3 className="about-card-title">My Technical Skills</h3>
+	<h3 className="about-card-title">MY TECHNICAL SKILLS</h3>
     <li className="about-card-content">HTML5 & CSS3</li>
 		<li className="about-card-content">JavaScript ES6+ & Node.js</li>
+    <li className="about-card-content">PostgreSQL & MongoDB</li>
+    <li className="about-card-content">Django & Flask</li>
     <li className="about-card-content">Python</li>
 	</div>
-	<div className="about-card">
-		<h3 className="about-card-title">CARD TITLE</h3>
-		<p className="about-card-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+  <div className="about-card">
+	<h3 className="about-card-title">MY PERSONAL SKILLS</h3>
+    <li className="about-card-content">Logical Problem Solver</li>
+		<li className="about-card-content">Proactive Listener</li>
+    <li className="about-card-content">Strong Teamwork Skills</li>
+    <li className="about-card-content">Adaptable & Dependable</li>
+    <li className="about-card-content">Effective Organizational Skills</li>
 	</div>
 	<div className="about-card">
-		<h3 className="about-card-title">CARD TITLE</h3>
-		<p className="about-card-content">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+		<h3 className="about-card-title">EXPERIENCE</h3>
+    <li className="about-card-content1">Trainee Full Stack Developer at futureproof</li>
+    <li className="about-card-content1">Graphic Designer at Silvertoad</li>
+    <li className="about-card-content1">Cricket Trading Assistant at Weaverdat</li>
 	</div>
 </div>
 </>
